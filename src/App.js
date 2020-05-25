aef3889279808c6475e60661b97c270f09eb1894
Anton <antonstalhandske@gmail.com>
2020-05-25 19:36:28 +0200

    Implemented a Navbar and fake auth check.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./components/layout/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
